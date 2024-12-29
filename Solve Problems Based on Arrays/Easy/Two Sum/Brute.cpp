38b744d34148b835f9ebf4248d29a2d49eef0154
Abhishek Prashant Chandurkar <140307256+Ccode104@users.noreply.github.com>
2024-12-29 17:51:09 +0530

    Update Brute.cpp

--- Solve Problems Based on Arrays/Easy/Two Sum/Brute.cpp
+++ Solve Problems Based on Arrays/Easy/Two Sum/Brute.cpp
@@ -1,1 +1,29 @@
+string read(int n, vector<int> book, int target)
+{
+    // Write your code here.
 
+    //Brute Force - Try all possible pairs
+
+    //Do not repeat the same pair
+
+    bool pairExists=false;
+    string ret_val = "NO";
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(book[i]+book[j]==target)
+            {
+                pairExists = true ;
+            }
+        }
+    }
+
+    if(pairExists==true)
+    {
+        ret_val = "YES";
+    }
+    
+    return ret_val;
+}
