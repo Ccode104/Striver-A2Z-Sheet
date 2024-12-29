0e513db7e9667cda397ab85b2b3d2dda41dfbd2b
Abhishek Prashant Chandurkar <140307256+Ccode104@users.noreply.github.com>
2024-12-29 18:30:06 +0530

    Update O3.cpp

--- Solve Problems Based on Arrays/Medium/Two Sum/O3.cpp
+++ Solve Problems Based on Arrays/Medium/Two Sum/O3.cpp
@@ -1,1 +1,43 @@
+#include<unordered_map>
+string read(int n, vector<int> book, int target)
+{
+    //Better approach
+    //Use Hashing 
+    //Search for target - book[i]
+    //Time Complexity is O(nlogn) - map has w.c O(logn)
+    //Space Complexity is O(n)
 
+    map<int,int> index;
+    int diff;
+    bool pairExists = false;
+    int flag=1;
+    string ret_val = "NO";
+    int i=0;
+
+    while((i<n)&&(flag!=0))
+    {
+        diff = target - book[i];
+        
+        if(index.find(diff)!=index.end())
+        {
+            //Found the required difference in the books
+
+            pairExists = true;
+            flag = 0; //break the loop
+        }
+        else
+        {
+            //Add the pair of book[i],i in index
+
+            index[book[i]] = i;
+        }
+        i = i + 1;
+    }
+
+    if(pairExists == true)
+    {
+        ret_val = "YES";
+    }
+
+    return ret_val;
+}
